6e8edc9597a241ea64ec396e52a4e7bd754f422a
yair-arboknot <yair@arborknot.io>
2022-02-07 17:15:36 +0200

    Added contact us withoud button.

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,14 @@
 import AboutUs from './about_us'
+import ContactUs from './contact_us/contactUs'
 import Head from './head'
+
 
 const Home = () => {
 
     return <>
         <Head/>
         <AboutUs/>
+        <ContactUs/>
     </>
 }
 
